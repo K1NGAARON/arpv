71cf0e933f08a42e4c8026e1f6ed708a53005ece
K1NGAARON <gerardaaron1997@gmail.com>
2023-10-03 10:24:01 +0200

    feedback on the homepage

--- home/js/script.js
+++ home/js/script.js
@@ -1,16 +1,16 @@
 function changeReviews() {
     const reviews = [
         {
-            name: 'Karen',
-            review: 'Al verschillende keren beroep gedaan op ARPV voor werken in ons huis. Binnen deuren, hulp bij parket, afwerking trap, stalen deur, ... Altijd prijs/kwaliteit tiptop in orde.',
+            name: 'Kim',
+            review: 'Ik ben super tevreden over hoe Thomas mijn 2 bovenverdiepingen heeft gerenoveerd. Hij denkt mee over mogelijke oplossingen, hij levert kwaliteit en de communicatie liep altijd vlot. Sowieso zal hij de rest van mijn huis in de toekomst mogen aanpakken!!',
         },
         {
-            name: 'Bert',
-            review: 'Thomas & team hebben ons snel en professioneel geholpen bij het plaatsen van een valse wand met binnendeur. De werken werden tijdig en kwalitatief uitgevoerd.',
+            name: 'Gery',
+            review: 'Samenwerken met ARPV was super! De firma heeft onze zolder geïsoleerd en afgewerkt tot een mooie relaxruimte. Tevens heeft hij onze verwarming nagezien en verbeterd en de regenwatervoorziening aangepast. Er was hierbij steeds een vlotte communicatie en een duidelijke en correcte prijsofferte en prijssetting. De afgesproken timing werd gevolgd. Ook de afwerking is prima. Thomas gaf goede suggesties, steeds met aandacht voor kwaliteit én prijsbewust. Hij was heel betrokken bij ons hele project. En op en top vriendelijk!',
         },
         {
-            name: 'Christine',
-            review: 'Top service, top afwerking, heel tevreden over. Binnendeuren laten plaatsen en verandering van ingang. Kwaliteitsvol voor eerlijke prijs. Wij zullen zeker nog werken laten uitvoeren door dit bedrijf. Aanrader.',
+            name: 'Yoshi',
+            review: 'Wij werkten samen met ARPV voor een totaalrenovatie van onze woning. Vanaf de ruwbouw tot de afwerking van de ruimtes werden alle werken met veel verantwoordelijkheid en oog voor detail uitgevoerd. Naast de werken zelf konden we ook steeds terecht bij ARPV voor vragen of goede ideeën omtrent afwerking en opbouw. Kortom een aannemer die mee helpt werken en denken aan het eindresultaat. 1000x bedankt voor wat je ons gezin gegeven hebt :) Geen vaarwel, maar een tot ziens!',
         }
     ];
 
